30eb30ad3eefdcb048d73b49150bf4d4252b1ff1
Ale <deferrari.alessandro@gmail.com>
2023-06-05 12:35:08 +0200

    last commit

--- tools.py
+++ tools.py
@@ -2,7 +2,6 @@ import os
 import cv2
 from deepface.commons import functions
 import torchvision
-import torch
 
 def face_alignment(input_dir: str, output_dir: str):
     for file_name in os.listdir(input_dir):
@@ -29,60 +28,3 @@ def embeddings_calc(gallery_paths: list, model, device, data_dir: str) -> dict:
         embeddings[path] = model(image).detach().cpu().numpy()
     return embeddings
 
-
-# create a function to train the model
-def train_model(model, criterion, optimizer, scheduler, num_epochs, device, dataloaders, dataset_sizes):
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch + 1, num_epochs))
-        print('-' * 10)
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                scheduler.step()
-                model.train()  # Set model to training mode
-                print('Training...')
-            else:
-                model.eval()  # Set model to evaluate mode
-                print('Validating...')
-            running_loss = 0.0
-            # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-                # zero the parameter gradients
-                optimizer.zero_grad()
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    loss = criterion(outputs, labels)
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-            epoch_loss = running_loss / dataset_sizes[phase]
-            print('{} Loss: {:.4f}'.format(phase, epoch_loss))
-    return model
-
-# create the data loaders
-def create_dataloaders(data_dir: str, batch_size: int):
-    data_transforms = {
-        'train': torchvision.transforms.Compose([
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Resize((224, 224), antialias=True),
-            torchvision.transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-        ]),
-        'val': torchvision.transforms.Compose([
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Resize((224, 224), antialias=True),
-            torchvision.transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-        ]),
-    }
-    image_datasets = {x: torchvision.datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in
-                      ['train', 'val']}
-    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4)
-                   for x in ['train', 'val']}
-    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-    return dataloaders, dataset_sizes
